f9ea561e54301a69176a54ba742b6d2b1be1feb3
sjodiel <jodiel_santos@hotmail.com>
2020-11-10 12:46:02 -0300

    Create Profile Component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-
+import Profile from "./Profile"
 
 const Layout = ({ children }) => {
   
 
   return (
+    <>
+      <aside>
+        <Profile/>
+      </aside>
      <main>{children}</main>
+     </>
         
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,20 +8,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
-    <ul>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <Link to="/" activeStyle={{ color : "red" }}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about">About (Gatsby Link)</Link>
-      </li>
-    </ul>
-    
   </Layout>
 )
 
