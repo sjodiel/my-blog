4fb86de89d38ee74c9ba0d7b804e6338bcec0dd5
sjodiel <jodiel_santos@hotmail.com>
2020-11-18 10:39:16 -0300

    Create PostItem component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,29 @@
 import React from "react"
 
-
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
   </Layout>
 )
 
