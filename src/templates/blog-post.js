a50b12ed4a3552864ed47d1874bc53f21565f2f7
sjodiel <jodiel_santos@hotmail.com>
2020-12-01 15:21:18 -0300

    Add Comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,14 +4,14 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import RecommendedPosts from "../components/RecommendedPosts"
+import Comments from "../components/Comments"
 
 import * as S from "../components/Post/styled"
 
-const BlogPost = ({ data, pageContext}) => {
+const BlogPost = ({ data, pageContext }) => {
   const post = data.markdownRemark
   const next = pageContext.nextPost
   const previous = pageContext.previousPost
-
 
   return (
     <Layout>
@@ -27,6 +27,7 @@ const BlogPost = ({ data, pageContext}) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
       <RecommendedPosts next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
     </Layout>
   )
 }
@@ -34,6 +35,9 @@ const BlogPost = ({ data, pageContext}) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
